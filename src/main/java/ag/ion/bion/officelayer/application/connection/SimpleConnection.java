f0c85cf4ec73c1e18137db8164a1612fcc5f602e
eforest <volkert.ewald@gmx.de>
2022-04-11 09:42:37 +0200

    Removing TextHandler as it's infected with GPL v3

--- src/main/java/ag/ion/bion/officelayer/application/connection/SimpleConnection.java
+++ src/main/java/ag/ion/bion/officelayer/application/connection/SimpleConnection.java
@@ -14,18 +14,6 @@
  */
 package ag.ion.bion.officelayer.application.connection;
 
-/**
- *
- * @author anti43
- */
-import ag.ion.bion.officelayer.application.IOfficeApplication;
-import ag.ion.bion.officelayer.application.OfficeApplicationException;
-import ag.ion.bion.officelayer.application.OfficeApplicationRuntime;
-import ag.ion.bion.officelayer.desktop.IDesktopService;
-import ag.ion.bion.officelayer.document.IDocumentService;
-import ag.ion.noa.NOAException;
-
-import com.sun.star.auth.InvalidArgumentException;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -40,8 +28,21 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import com.sun.star.auth.InvalidArgumentException;
+
 /**
- *This class handles connections to remote and local OpenOffice installations
+ *
+ * @author anti43
+ */
+import ag.ion.bion.officelayer.application.IOfficeApplication;
+import ag.ion.bion.officelayer.application.OfficeApplicationException;
+import ag.ion.bion.officelayer.application.OfficeApplicationRuntime;
+import ag.ion.bion.officelayer.desktop.IDesktopService;
+import ag.ion.bion.officelayer.document.IDocumentService;
+import ag.ion.noa.NOAException;
+
+/**
+ * This class handles connections to remote and local OpenOffice installations
  */
 public class SimpleConnection {
 
@@ -61,44 +62,54 @@ public class SimpleConnection {
 
     /**
      * Creates a connection, depending on the current local config.
+     * 
      * @return
      */
     public synchronized static SimpleConnection getConnection() {
         Properties props = System.getProperties();
-        if (props.containsKey("OFFICE_HOST")) {
-            if (Connection == null) {
+        if ( props.containsKey( "OFFICE_HOST" ) ) {
+            if ( Connection == null ) {
                 try {
-                    Connection = new SimpleConnection(props.getProperty("OFFICE_HOST"), Integer.valueOf(props.getProperty("OFFICE_PORT")));
+                    Connection = new SimpleConnection(
+                        props.getProperty( "OFFICE_HOST" ),
+                        Integer.valueOf( props.getProperty( "OFFICE_PORT" ) ) );
 
-                } catch (Exception ex) {
-                    Logger.getLogger(SimpleConnection.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                catch ( Exception ex ) {
+                    Logger.getLogger( SimpleConnection.class.getName() ).log( Level.SEVERE, null, ex );
 
                 }
             }
             return Connection;
-        } else {
-            throw new UnsupportedOperationException("OpenOffice is not configured yet.");
+        }
+        else {
+            throw new UnsupportedOperationException( "OpenOffice is not configured yet." );
         }
     }
 
     /**
      * New NoaConnection instance, connects to OO using the given parameters.
-     * @param connectionString The connection String. Can be a <b>Path<b/> or an <b>IP<b/>
-     * @param port The port to connect to. A port value of zero (0) indicates a <b>local<b/> connection
+     * 
+     * @param connectionString The connection String. Can be a <b>Path</b> or an <b>IP</b>
+     * @param port The port to connect to. A port value of zero (0) indicates a <b>local</b> connection
      * @throws Exception If any Exception is thrown during the connection attempt
      */
     public SimpleConnection(String connectionString, int port) throws Exception {
-        if (connectionString != null && connectionString.length() > 1 && port <= 0) {
-            createLocalConnection(connectionString);
-        } else if (connectionString != null && connectionString.length() > 1 && port > 0 && port < 9999) {
-            createServerConnection(connectionString, port);
-        } else {
-            throw new Exception("Connection not possible with the given parameters: [" + connectionString + ":" + port + "]");
+        if ( connectionString != null && connectionString.length() > 1 && port <= 0 ) {
+            createLocalConnection( connectionString );
+        }
+        else if ( connectionString != null && connectionString.length() > 1 && port > 0 && port < 9999 ) {
+            createServerConnection( connectionString, port );
+        }
+        else {
+            throw new Exception(
+                "Connection not possible with the given parameters: [" + connectionString + ":" + port + "]" );
         }
     }
 
     /**
      * Creates a new connection
+     * 
      * @param host
      * @param port
      * @return
@@ -106,76 +117,77 @@ public class SimpleConnection {
      * @throws NOAException
      * @throws InvalidArgumentException
      */
-    private synchronized boolean createServerConnection(String host, int port) throws OfficeApplicationException, NOAException, InvalidArgumentException {
-        if (host != null && port > 0) {
+    private synchronized boolean createServerConnection(String host, int port)
+        throws OfficeApplicationException, NOAException, InvalidArgumentException {
+        if ( host != null && port > 0 ) {
             Map<String, String> configuration = new HashMap<String, String>();
-            configuration.put(IOfficeApplication.APPLICATION_TYPE_KEY,
-                    IOfficeApplication.REMOTE_APPLICATION);
-            configuration.put(IOfficeApplication.APPLICATION_HOST_KEY, host.replace("http://", ""));
-            configuration.put(IOfficeApplication.APPLICATION_PORT_KEY, String.valueOf(port));
+            configuration.put( IOfficeApplication.APPLICATION_TYPE_KEY, IOfficeApplication.REMOTE_APPLICATION );
+            configuration.put( IOfficeApplication.APPLICATION_HOST_KEY, host.replace( "http://", "" ) );
+            configuration.put( IOfficeApplication.APPLICATION_PORT_KEY, String.valueOf( port ) );
 
-            officeAplication =
-                    OfficeApplicationRuntime.getApplication(configuration);
-            officeAplication.setConfiguration(configuration);
+            officeAplication = OfficeApplicationRuntime.getApplication( configuration );
+            officeAplication.setConfiguration( configuration );
             try {
                 officeAplication.activate();
-            } catch (Exception officeApplicationException) {
+            }
+            catch ( Exception officeApplicationException ) {
                 try {
-                    Thread.sleep(6666);
-                } catch (InterruptedException ex) {
+                    Thread.sleep( 6666 );
+                }
+                catch ( InterruptedException ex ) {
                 }
                 try {
                     officeAplication.activate();
-                } catch (OfficeApplicationException officeApplicationException1) {
-                    Logger.getLogger(SimpleConnection.class.getName()).log(Level.SEVERE, null, officeApplicationException1);
+                }
+                catch ( OfficeApplicationException officeApplicationException1 ) {
+                    Logger
+                          .getLogger( SimpleConnection.class.getName() )
+                          .log( Level.SEVERE, null, officeApplicationException1 );
                 }
             }
-            documentService =
-                    officeAplication.getDocumentService();
-            desktopService =
-                    officeAplication.getDesktopService();
-            setType(TYPE_REMOTE);
-        } else {
-            throw new InvalidArgumentException("Host cannot be null and port must be > 0: " + host + ":" + port);
+            documentService = officeAplication.getDocumentService();
+            desktopService = officeAplication.getDesktopService();
+            setType( TYPE_REMOTE );
+        }
+        else {
+            throw new InvalidArgumentException( "Host cannot be null and port must be > 0: " + host + ":" + port );
         }
 
         return true;
     }
 
     /**
-     *
      * @param OOOPath
      * @return
      * @throws OfficeApplicationException
      * @throws NOAException
      * @throws InvalidArgumentException
      */
-    private synchronized boolean createLocalConnection(String OOOPath) throws OfficeApplicationException, NOAException, InvalidArgumentException {
-        if (OOOPath != null) {
+    private synchronized boolean createLocalConnection(String OOOPath)
+        throws OfficeApplicationException, NOAException, InvalidArgumentException {
+        if ( OOOPath != null ) {
             Map<String, String> configuration = new HashMap<String, String>();
-            configuration.put(IOfficeApplication.APPLICATION_HOME_KEY, OOOPath);
-            configuration.put(IOfficeApplication.APPLICATION_TYPE_KEY,
-                    IOfficeApplication.LOCAL_APPLICATION);
+            configuration.put( IOfficeApplication.APPLICATION_HOME_KEY, OOOPath );
+            configuration.put( IOfficeApplication.APPLICATION_TYPE_KEY, IOfficeApplication.LOCAL_APPLICATION );
 
-            officeAplication =
-                    OfficeApplicationRuntime.getApplication(configuration);
-            officeAplication.setConfiguration(configuration);
+            officeAplication = OfficeApplicationRuntime.getApplication( configuration );
+            officeAplication.setConfiguration( configuration );
             officeAplication.activate();
-            documentService =
-                    officeAplication.getDocumentService();
-            desktopService =
-                    officeAplication.getDesktopService();
+            documentService = officeAplication.getDocumentService();
+            desktopService = officeAplication.getDesktopService();
 
-            setType(TYPE_LOCAL);
-        } else {
-            throw new InvalidArgumentException("Path to OO cannot be null: " + OOOPath);
+            setType( TYPE_LOCAL );
+        }
+        else {
+            throw new InvalidArgumentException( "Path to OO cannot be null: " + OOOPath );
         }
 
         return true;
     }
 
     /**
-     *  -1 indicates no connection.
+     * -1 indicates no connection.
+     * 
      * @return the type
      */
     public synchronized int getType() {
@@ -204,36 +216,36 @@ public class SimpleConnection {
     }
 
     /**
-     * Tries to start OO in headless server mode. <code>Give it at least 3-4 seconds before attempting to use the server.</code>
+     * Tries to start OO in headless server mode.
+     * <code>Give it at least 3-4 seconds before attempting to use the server.</code>
+     * 
      * @param path The path where the OO installation resides
      * @param port The port the server shall listen to
      * @throws IOException
      */
     public synchronized static void startOOServerIfNotRunning(final String path, final int port) {
         final Properties props = System.getProperties();
-        final String command = path.replace("\\", "\\\\") + File.separator + props.getProperty("OFFICE_BINARY_FOLDER") + File.separator + "soffice" + " "
-                + "-headless" + " "
-                + "-nofirststartwizard" + " "
-                + "-norestore" + " "
-                + "-nolockcheck" + " "
-                + "-nocrashreport" + " "
-                + "-nodefault" + " "
-                + "-accept='socket,host=0.0.0.0,port=" + port + ";urp;StarOffice.Service'";
+        final String command = path.replace( "\\", "\\\\" ) + File.separator
+            + props.getProperty( "OFFICE_BINARY_FOLDER" ) + File.separator + "soffice" + " " + "-headless" + " "
+            + "-nofirststartwizard" + " " + "-norestore" + " " + "-nolockcheck" + " " + "-nocrashreport" + " "
+            + "-nodefault" + " " + "-accept='socket,host=0.0.0.0,port=" + port + ";urp;StarOffice.Service'";
 
         try {
-            SocketAddress addr = new InetSocketAddress("127.0.0.1", port);
+            SocketAddress addr = new InetSocketAddress( "127.0.0.1", port );
             Socket socket = new Socket();
-            socket.connect(addr, 100);
-            throw new UnsupportedOperationException("Port " + port + " is already in use :-/. Not going to start OO here.");
-        } catch (IOException iOException) {
-            //nothing is running here
+            socket.connect( addr, 100 );
+            throw new UnsupportedOperationException(
+                "Port " + port + " is already in use :-/. Not going to start OO here." );
+        }
+        catch ( IOException iOException ) {
+            // nothing is running here
         }
 
-        final ProcessBuilder builder = new ProcessBuilder(command);
+        final ProcessBuilder builder = new ProcessBuilder( command );
 
         Map<String, String> environment = builder.environment();
-        environment.put("path", ";"); // Clearing the path variable;
-        environment.put("path", path.replace("\\", "\\\\") + File.pathSeparator);
+        environment.put( "path", ";" ); // Clearing the path variable;
+        environment.put( "path", path.replace( "\\", "\\\\" ) + File.pathSeparator );
 
         Runnable runnable = new Runnable() {
 
@@ -242,19 +254,20 @@ public class SimpleConnection {
                 try {
                     Process oos = builder.start();
                     InputStream is = oos.getErrorStream();
-                    InputStreamReader isr = new InputStreamReader(is);
-                    BufferedReader br = new BufferedReader(isr);
+                    InputStreamReader isr = new InputStreamReader( is );
+                    BufferedReader br = new BufferedReader( isr );
                     String line;
-                    while ((line = br.readLine()) != null) {
-                        Logger.getLogger(SimpleConnection.class.getName()).log(Level.INFO, null, line);
+                    while ( ( line = br.readLine() ) != null ) {
+                        Logger.getLogger( SimpleConnection.class.getName() ).log( Level.INFO, null, line );
                     }
-                } catch (IOException ex) {
-                    Logger.getLogger(SimpleConnection.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                catch ( IOException ex ) {
+                    Logger.getLogger( SimpleConnection.class.getName() ).log( Level.SEVERE, null, ex );
                 }
 
             }
         };
-        new Thread(runnable).start();
+        new Thread( runnable ).start();
 
     }
 }
